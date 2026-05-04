11111ac9393f715a52ee66e8905d7c9be3ac291b
BushrangerLabs <bushrangerlabs@users.noreply.github.com>
2026-05-04 18:10:22 +1000

    Allow multiple Canvas Display devices in HA (remove single_instance_allowed)

--- custom_components/canvas_display/config_flow.py
+++ custom_components/canvas_display/config_flow.py
@@ -14,9 +14,6 @@ class CanvasDisplayConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
-
         errors: dict[str, str] = {}
 
         if user_input is not None:
